37190b3233fc0957e3d9722c880a031d1432592b
I'm Liam <liamhammett@gmail.com>
2017-03-14 12:47:52 +0000

    Added `exists` option, debug logs, default content value and fixed animation value

--- jquery.scrollup.js
+++ jquery.scrollup.js
@@ -1,16 +1,27 @@
+/*! jquery.scrollup v1.0.0, @license MIT */
+
+/*
+|--------------------------------------------------------------------------
+| jQuery Scroll Up
+|--------------------------------------------------------------------------
+|
+| A jQuery plugin to recreate the commonly used "pull up to refresh" found
+| in mobile devices, but this time for the desktop browser.
+|
+*/
+
 (function ( $ ) {
   $.fn.scrollup = function(selector, options) {
 
-    if (typeof options.content == 'undefined') {
-        console.log('jQuery.scrollup could not be initialised, a "content" value must be set.');
-        return;
+    if (typeof options.content == 'undefined' || !options.content) {
+        options.content = '';
     }
 
     if (typeof options.class == 'undefined' || !options.class) {
         options.class = 'scrollup';
     }
 
-    if (typeof options.animationSpeed == 'undefined' || options.animationSpeed != 'fast' || options.animationSpeed != 'slow') {
+    if (typeof options.animationSpeed == 'undefined' || options.animationSpeed.toLowerCase() != 'fast' || options.animationSpeed.toLowerCase() != 'slow') {
         options.animationSpeed = 'fast';
     }
 
@@ -23,23 +34,30 @@
 
         if ($(this).has('.'+options.class).length == 0) {
 
+            if (options.debug) { console.log('jQuery.scrollup object is being created.'); }
             if (options.create) { options.create(); }
 
             var scrollupElement = $('<div class="'+options.class+'">'+options.content+'</div>');
 
             scrollupElement.hide().prependTo(selector).show(options.animationSpeed);
 
+            if (options.debug) { console.log('jQuery.scrollup object has been created.'); }
             if (options.created) { options.created(); }
 
             if (options.timeout) {
                 setTimeout(function() {
+                    if (options.debug) { console.log('jQuery.scrollup object is being removed.'); }
                     if (options.remove) { options.remove(); }
                     scrollupElement.hide(options.animationSpeed, function() {
                         scrollupElement.remove();
+                        if (options.debug) { console.log('jQuery.scrollup object has been removed.'); }
                         if (options.removed) { options.removed(); }
                     });
                 }, options.timeout);
             }
+        } else {
+            if (options.debug) { console.log('jQuery.scrollup triggered, but object already exists.'); }
+            if (options.exists) { options.exists(); }
         }
       }
     });
