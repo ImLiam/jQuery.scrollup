7816038ec18b288b8cccfc68b5d1a9e64a51c9b0
I'm Liam <liamhammett@gmail.com>
2017-03-14 15:32:34 +0000

    Added two new methods: .remove() and .options()

--- jquery.scrollup.js
+++ jquery.scrollup.js
@@ -1,29 +1,11 @@
 /*! jquery.scrollup v1.0.0, @license MIT */
 
-/*
-|--------------------------------------------------------------------------
-| jQuery Scroll Up
-|--------------------------------------------------------------------------
-|
-| A jQuery plugin to recreate the commonly used "pull up to refresh" found
-| in mobile devices, but this time for the desktop browser.
-|
-*/
-
 (function ( $ ) {
+
+
   $.fn.scrollup = function(selector, options) {
 
-    if (typeof options.content == 'undefined' || !options.content) {
-        options.content = '';
-    }
-
-    if (typeof options.class == 'undefined' || !options.class) {
-        options.class = 'scrollup';
-    }
-
-    if (typeof options.animationSpeed == 'undefined' || options.animationSpeed.toLowerCase() != 'fast' || options.animationSpeed.toLowerCase() != 'slow') {
-        options.animationSpeed = 'fast';
-    }
+    options = $(this).scrollupOptions(options);
 
     if (options.debug) {
         console.log('jQuery.scrollup initialised on selector: "'+selector+'"');
@@ -32,7 +14,7 @@
     $(selector).on('wheel', function(e) {
       if ($(this).scrollTop() == 0 && e.originalEvent.wheelDelta >= 0) {
 
-        if ($(this).children('.'+options.class).length == 0) {
+        if ($(this).children('.' + options.class).length == 0) {
 
             if (options.debug) { console.log('jQuery.scrollup object is being created.'); }
             if (options.create) { options.create(); }
@@ -46,21 +28,72 @@
 
             if (options.timeout) {
                 setTimeout(function() {
-                    if (options.debug) { console.log('jQuery.scrollup object is being removed.'); }
-                    if (options.remove) { options.remove(); }
-                    scrollupElement.hide(options.animationSpeed, function() {
-                        scrollupElement.remove();
-                        if (options.debug) { console.log('jQuery.scrollup object has been removed.'); }
-                        if (options.removed) { options.removed(); }
-                    });
+                    $(this).scrollupRemove(selector, options);
                 }, options.timeout);
             }
+
         } else {
+
             if (options.debug) { console.log('jQuery.scrollup triggered, but object already exists.'); }
             if (options.exists) { options.exists(); }
+
         }
       }
     });
 
   }
+  
+  
+  
+  $.fn.scrollupRemove = function(selector, options) {
+
+    options = $(this).scrollupOptions(options);
+    
+    if (options.debug) { console.log('jQuery.scrollup object is being removed.'); }
+    if (options.remove) { options.remove(); }
+    
+    var scrollupElement = $(selector).children('.' + options.class);
+    
+    if (scrollupElement.length) {
+        $(scrollupElement).hide(options.animationSpeed, function() {
+            $(scrollupElement).remove();
+            if (options.debug) { console.log('jQuery.scrollup object has been removed.'); }
+            if (options.removed) { options.removed(); }
+            return true;
+        });
+    } else {
+        if (options.debug) {
+            console.log('jQuery.scrollup did not find a scrollup object to remove.');
+        }
+        return false;
+    }
+
+  }
+  
+  
+  
+  $.fn.scrollupOptions = function(options) {
+
+    if (typeof options == 'undefined') {
+        options = {};
+    }
+
+    if (typeof options.content == 'undefined') {
+        options.content = '';
+    }
+
+    if (typeof options.class == 'undefined') {
+        options.class = 'scrollup';
+    }
+
+    if (typeof options.animationSpeed == 'undefined' || options.animationSpeed.toLowerCase() != 'fast' || options.animationSpeed.toLowerCase() != 'slow') {
+        options.animationSpeed = 'fast';
+    }
+    
+    return options;
+
+  }
+  
+
+
 }( jQuery ));
